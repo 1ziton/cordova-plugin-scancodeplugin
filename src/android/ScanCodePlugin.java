d4493945b99d0e78b7d57080e0d79cd23b230d66
Txiaomo <920449619@qq.com>
2017-09-30 17:57:58 +0800

    Update ScanCodePlugin.java

--- src/android/ScanCodePlugin.java
+++ src/android/ScanCodePlugin.java
@@ -3,30 +3,71 @@ package cordova.plugin.ScanCodePlugin;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.yle.borcodescan.app.CaptureActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.xml.validation.TypeInfoProvider;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class ScanCodePlugin extends CordovaPlugin {
-private CallbackContext callbackContext;
+    private CallbackContext callbackContext;
     private static final int REQUEST_QRCODE = 0x01;
+    private static final int PERMISSION_DENIED_ERROR=20;
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
         if (action.equals("scan")) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                //高编译版本 23含以上
+                if (ContextCompat.checkSelfPermission(cordova.getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    Toast toast=Toast.makeText(cordova.getActivity(),"请在系统设置中打开照相机权限",Toast.LENGTH_LONG);
+                    showtoast(toast,4000);
+                }
+            }
             Intent intent = new Intent(cordova.getActivity(), CaptureActivity.class);
             cordova.startActivityForResult(this, intent, REQUEST_QRCODE);
             return true;
         }
         return false;
+    }
+
+    public void showtoast(final Toast toast,final int cnt){
+        final Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                toast.show();
+            }
+        },0,4000);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                toast.cancel();
+                timer.cancel();
+            }
+        },cnt);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
